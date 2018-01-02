c024b1e1530dae0e85b9f7d033a64c76fc25d177
Craig Weber <crgwbr@gmail.com>
2018-01-02 13:41:39 -0500

    Fix deprecation warnings

--- src/vaulthelpers/tests/test_config.py
+++ src/vaulthelpers/tests/test_config.py
@@ -6,26 +6,26 @@ class EnvironmentConfigTest(VaultHelperTest):
 
     def test_dictionary_keys(self):
         config = vaulthelpers.EnvironmentConfig('secret/vaulthelpers-sandbox/django-settings')
-        self.assertEquals(config['SECRET_KEY'], 'my-django-secret-key')
-        self.assertEquals(config['SOME_API_KEY'], 'some-secret-api-key')
+        self.assertEqual(config['SECRET_KEY'], 'my-django-secret-key')
+        self.assertEqual(config['SOME_API_KEY'], 'some-secret-api-key')
 
 
     def test_get_method(self):
         config = vaulthelpers.EnvironmentConfig('secret/vaulthelpers-sandbox/django-settings')
-        self.assertEquals(config.get('SECRET_KEY'), 'my-django-secret-key')
-        self.assertEquals(config.get('SOME_API_KEY'), 'some-secret-api-key')
+        self.assertEqual(config.get('SECRET_KEY'), 'my-django-secret-key')
+        self.assertEqual(config.get('SOME_API_KEY'), 'some-secret-api-key')
 
 
     def test_default_for_nonexisting_key(self):
         config = vaulthelpers.EnvironmentConfig('secret/vaulthelpers-sandbox/django-settings')
-        self.assertEquals(config.get('MISSING_KEY'), None)
-        self.assertEquals(config.get('MISSING_KEY', 'my-default'), 'my-default')
+        self.assertEqual(config.get('MISSING_KEY'), None)
+        self.assertEqual(config.get('MISSING_KEY', 'my-default'), 'my-default')
 
 
     def test_environment_variable_fallback(self):
         config = vaulthelpers.EnvironmentConfig('secret/vaulthelpers-sandbox/django-settings')
-        self.assertEquals(config['DATABASE_OWNERROLE'], 'vaulthelpers')
+        self.assertEqual(config['DATABASE_OWNERROLE'], 'vaulthelpers')
         with self.assertRaises(KeyError):
             config['MISSING_KEY']
-        self.assertEquals(config.get('DATABASE_OWNERROLE'), 'vaulthelpers')
-        self.assertEquals(config.get('MISSING_KEY'), None)
+        self.assertEqual(config.get('DATABASE_OWNERROLE'), 'vaulthelpers')
+        self.assertEqual(config.get('MISSING_KEY'), None)

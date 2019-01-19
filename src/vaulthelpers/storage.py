b6c894634aa3783018a496b6d056049e17076fd3
Craig Weber <crgwbr@gmail.com>
2019-01-18 20:21:31 -0500

    Fix issue in sotrage connector when DEFAULT_SESSION is None

--- src/vaulthelpers/storage.py
+++ src/vaulthelpers/storage.py
@@ -7,7 +7,7 @@ class S3Boto3Storage(BaseS3Boto3Storage):
     def connection(self):
         connection = getattr(self._connections, 'connection', None)
         if connection is None:
-            self._connections.connection = boto3.DEFAULT_SESSION.resource('s3',
+            self._connections.connection = boto3.resource('s3',
                 region_name=self.region_name,
                 use_ssl=self.use_ssl,
                 endpoint_url=self.endpoint_url,
